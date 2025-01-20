6f9ebe15e5a942f56b8a6c8a507f6bd0c75c4503
Vitaliy Zarubin <v.zarubin@omp.ru>
2025-01-20 20:17:07 +0300

    Start clap.

--- src/main.rs
+++ src/main.rs
@@ -1,162 +1,156 @@
-use std::io;
+use clap::Parser;
+use clap::Subcommand;
+use clap::Args;
+use colored::Colorize;
 
-use ratatui_image::{picker::Picker, protocol::StatefulProtocol, Resize, StatefulImage};
-use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
-use ratatui::{
-    buffer::Buffer,
-    layout::{Alignment, Rect},
-    style::{Modifier, Style, Stylize},
-    symbols::border,
-    text::{Line, Span},
-    widgets::{Block, Paragraph, Widget, Wrap},
-    DefaultTerminal,
-    Frame,
-};
+fn get_about() -> String {
 
-mod utils;
+    let link1 = "https://developer.auroraos.ru/doc/sdk".blue().to_string();
+    let link2 = "https://omprussia.gitlab.io/flutter/flutter".blue().to_string();
+    let link3 = "https://developer.auroraos.ru/doc/sdk/psdk".blue().to_string();
 
-// Check language, functions
+    return format!(r#"
+
+The application allows you to install tools for working with the Aurora OS
+and simplifies working with them. More details about the tools can be found
+on the documentation page:
+
+Aurora SDK   {}
+Flutter SDK  {}
+Platform SDK {}
+
+This is a third party tool written by enthusiasts!"#, link1, link2, link3);
+}
+
+pub fn get_styles() -> clap::builder::Styles {
+    clap::builder::Styles::styled()
+        .usage(
+            anstyle::Style::new()
+                .bold()
+                .underline()
+                .fg_color(Some(anstyle::Color::Ansi(anstyle::AnsiColor::White))),
+        )
+        .header(
+            anstyle::Style::new()
+                .bold()
+                .underline()
+                .fg_color(Some(anstyle::Color::Ansi(anstyle::AnsiColor::Green))),
+        )
+        .literal(
+            anstyle::Style::new().fg_color(Some(anstyle::Color::Ansi(anstyle::AnsiColor::BrightCyan))),
+        )
+        .invalid(
+            anstyle::Style::new()
+                .bold()
+                .fg_color(Some(anstyle::Color::Ansi(anstyle::AnsiColor::Red))),
+        )
+        .error(
+            anstyle::Style::new()
+                .bold()
+                .fg_color(Some(anstyle::Color::Ansi(anstyle::AnsiColor::Red))),
+        )
+        .valid(
+            anstyle::Style::new()
+                .bold()
+                .underline()
+                .fg_color(Some(anstyle::Color::Ansi(anstyle::AnsiColor::Green))),
+        )
+        .placeholder(
+            anstyle::Style::new().fg_color(Some(anstyle::Color::Ansi(anstyle::AnsiColor::Yellow))),
+        )
+}
+
+/// A fictional versioning CLI
+#[derive(Debug, Parser)] // requires `derive` feature
+#[command(about = get_about(), long_about = None)]
+#[command(args_conflicts_with_subcommands = true)]
+#[command(disable_help_flag = true)]
+#[command(styles=get_styles())]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+
+    /// Specify config path.
+    #[arg(short='c', long)]
+    config: Option<String>,
+
+    /// Clear cached data.
+    #[arg(short='r', long, default_value_t = false)]
+    remove_cache: bool,
+
+    /// Show the version and exit.
+    #[arg(short='v', long, default_value_t = false)]
+    version: bool,
+
+    /// Show this message and exit.
+    #[clap(short='h', long, action = clap::ArgAction::HelpLong)]
+    help: Option<bool>,
+}
+
+// /// Aurora Bot | Client - приложение обеспечивающие работу с инфраструктурой ОС Аврора, связь с сервером, реализующем умную командную строку и удалённое взаимодействие 😎
+// #[derive(Parser, Debug)]
+// #[command(version)]
+// struct Args {
+//     /// Just tell me what to do
+//     #[arg(short, long)]
+//     ai: String,
+
+//     /// Search data for Aurora Dataset
+//     #[arg(short, long)]
+//     search: String,
+
+//     // /// Provides additional information
+//     // #[arg(short, long, default_value_t = false)]
+//     // verbose: bool,
+
+//     // #[command(subcommand)]
+//     // command: Groups,
+// }
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Application Programming Interface.
+    Api {},
+    /// Applications available for installation.
+    Apps {},
+    /// Work with the device.
+    Device {},
+    /// Work with the emulator virtualbox.
+    Emulator {},
+    /// Work with Flutter for Aurora OS.
+    Flutter {},
+    /// Work with Platform Aurora SDK.
+    Psdk {},
+    /// Work with Aurora SDK.
+    Sdk {},
+    /// Additional application settings.
+    Settings {},
+    /// Работа с Visual Studio Code.
+    Vscode {},
+
+    // Stash(StashArgs),
+}
+
+#[derive(Debug, Args)]
+#[command(args_conflicts_with_subcommands = true)]
+#[command(flatten_help = true)]
+struct StashArgs {
+    /// Provides additional information
+    #[arg(short, long, default_value_t = false)]
+    verbose: bool,
+}
+
 fn main() {
-    utils::run_tests();
-}
+    let _ = Cli::parse();
 
-// TUI
-#[allow(dead_code)]
-fn run_tui() -> Result<(), Box<dyn std::error::Error>> {
-    let mut terminal = ratatui::init();
-    let picker = Picker::from_query_stdio().unwrap();
-    let image_source = image::ImageReader::open("/home/keygenqt/Documents/Home/Projects/aurora-bot/assets/6048909d-cb71-4d59-964b-15e64d1bc9af.jpeg")?.decode()?;
-    let image = picker.new_resize_protocol(image_source.clone());
-    let mut app = App { image, is_image: false, exit: false };
-    let app_result = app.run(&mut terminal);
-    ratatui::restore();
-    app_result
-}
 
-pub struct App {
-    image: StatefulProtocol,
-    is_image: bool,
-    exit: bool,
-}
 
-impl App {
-    /// runs the application's main loop until the user quits
-    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> Result<(), Box<dyn std::error::Error>> {
-        while !self.exit {
-            if self.is_image {
-                terminal.draw(|frame| self.draw_image(frame))?;
-            } else {
-                terminal.draw(|frame| self.draw(frame))?;
-            }
-            self.handle_events()?;
-        }
-        Ok(())
-    }
+    // if m.is_present("help") {
+    //     cli.print_help().unwrap();
+    //     return;
+    // }
 
-    fn draw_image(&mut self, frame: &mut Frame) {
-        let title = Line::from(" Image ".bold());
-        let instructions = Line::from(vec![
-            " Back ".into(),
-            "<B> ".blue().bold(),
-            " Quit ".into(),
-            "<Q> ".blue().bold(),
-        ]);
-        let block = Block::bordered()
-            .title(title.centered())
-            .title_bottom(instructions.centered())
-            .border_set(border::THICK);
-
-        // Image
-        let resize = Resize::Scale(None);
-        let image = StatefulImage::default().resize(resize);
-        let inner_area = block.inner(frame.area());
-        frame.render_stateful_widget(image, inner_area, &mut self.image);
-
-        // Block
-        frame.render_widget(block, frame.area());
-
-    }
-
-    fn draw(&self, frame: &mut Frame) {
-        frame.render_widget(self, frame.area());
-    }
-
-    /// updates the application's state based on user input
-    fn handle_events(&mut self) -> io::Result<()> {
-        match event::read()? {
-            // it's important to check that the event is a key press event as
-            // crossterm also emits key release and repeat events on Windows.
-            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
-                self.handle_key_event(key_event)
-            }
-            _ => {}
-        };
-        Ok(())
-    }
-
-    fn handle_key_event(&mut self, key_event: KeyEvent) {
-        match key_event.code {
-            KeyCode::Char('b') => self.hide_image(),
-            KeyCode::Char('i') => self.show_image(),
-            KeyCode::Char('q') => self.exit(),
-            _ => {}
-        }
-    }
-
-    fn hide_image(&mut self) {
-        self.is_image = false;
-    }
-
-    fn show_image(&mut self) {
-        self.is_image = true;
-    }
-
-    fn exit(&mut self) {
-        self.exit = true;
-    }
-}
-
-impl Widget for &App {
-    fn render(self, area: Rect, buf: &mut Buffer) {
-        let title = Line::from(" Aurora Bot ".bold());
-        let instructions = Line::from(vec![
-            " Image ".into(),
-            "<I> ".blue().bold(),
-            " Quit ".into(),
-            "<Q> ".blue().bold(),
-        ]);
-        let block = Block::bordered()
-            .title(title.centered())
-            .title_bottom(instructions.centered())
-            .border_set(border::THICK);
-
-        let post_title = Span::styled(" 🎞️ Картографические библиотеки MFW для ОС Аврора.", Style::default().green().add_modifier(Modifier::BOLD));
-        let body = Span::styled(" Делимся с вами записью выступления Дмитрия Лапшина, старшего инженера-разработчика ОМП, в котором он рассказывает про картографические библиотеки MFW для ОС Аврора 🗺", Style::default());
-        let link = Span::styled(" 🔗 Смотреть видео", Style::default().blue());
-
-        let author_raw = match utils::format(" Виталий Зарубин, 17 января 2025.") {
-            Ok(it) => it,
-            Err(_) => "Error format",
-        };
-
-        let author = Span::styled(author_raw , Style::default().italic().gray());
-
-        let post: Vec<Line<'_>> = vec![
-            post_title.into(),
-            Span::from("").into(),
-            body.into(),
-            Span::from("").into(),
-            link.into(),
-            Span::from("").into(),
-            Span::from("⭐⭐⭐⭐⭐  5.00").into(),
-            Span::from("").into(),
-            author.into(),
-        ];
-
-        Paragraph::new(post)
-            .alignment(Alignment::Left)
-            .block(block)
-            .wrap(Wrap { trim: true })
-            .render(area, buf);
-    }
+    // for _ in 0..cli.count {
+    //     println!("Hello {}!", cli.name);
+    // }
 }
